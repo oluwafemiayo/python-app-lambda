7404733c8ae56ae443eba0ce4c3239d44fb22716
oluwafemiayo <olufemi123ayo@gmail.com>
2023-01-13 18:18:30 +0100

    new message

--- python/hello-python.py
+++ python/hello-python.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    message = 'hello from Lagos Nigeria {} !' .format(event['key1'])
+    message = 'hello  {} !' .format(event['key1'])
     return {
         'message': message
     }
